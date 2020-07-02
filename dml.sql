b2e7c83d15ecbc96aa6f1166110c8ca586ec145c
sympathyzz <51390756+sympathyzz@users.noreply.github.com>
2020-07-02 15:19:12 +0800

    Update dml.sql

--- dml.sql
+++ dml.sql
@@ -1,7 +1,8 @@
 -- Insert record
-
+insert into `subject` values(1003,'History','Miss Li','the exam is easy');
 -- Revise record
-
+update `subject` set subject='English' where id=1003;
 --  Delete record
-
+delete FROM `subject` where id=1003;
 -- Search record
+select * from `subject`;
